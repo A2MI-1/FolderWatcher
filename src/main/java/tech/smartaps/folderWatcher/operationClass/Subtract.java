a2a068166fd364c44aebc9970d590b5018b244cd
Marvyng Ismael <assamouac20@gmail.com>
2025-06-16 12:11:43 +0000

    refactor: refactored Subtract.java class to execute Subtraction

--- src/main/java/tech/smartaps/folderWatcher/operationClass/Subtract.java
+++ src/main/java/tech/smartaps/folderWatcher/operationClass/Subtract.java
@@ -7,14 +7,10 @@ import java.util.List;
 public class Subtract implements ICalculator {
     @Override
     public Double compute(List<Double> operationParameters) {
-        // get first element and remove it from parameters
-        /* Double result = operationParameters.get(0);
-        operationParameters.remove(0);
+        Double result = 0.0;
         for(Double d : operationParameters) {
-            result -= d;
+            result += d;
         }
         return result;
-        */
-        return 0.0;
     }
 }
