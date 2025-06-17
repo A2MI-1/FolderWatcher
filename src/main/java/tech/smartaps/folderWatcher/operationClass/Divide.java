57edcc835d7631251921d0e1f84461f83378f7cd
Marvyng Ismael <assamouac20@gmail.com>
2025-06-17 09:29:37 +0000

    fix: fixed compute method in divide class for avoiding division by 0

--- src/main/java/tech/smartaps/folderWatcher/operationClass/Divide.java
+++ src/main/java/tech/smartaps/folderWatcher/operationClass/Divide.java
@@ -10,7 +10,9 @@ public class Divide implements ICalculator {
     public Double compute(List<Double> operationParameters) {
         Double result = operationParameters.get(0);
         for(int i = 1; i < operationParameters.size(); i++) {
-            result /= operationParameters.get(i);
+            Double param = operationParameters.get(i);
+            if(Double.compare(param, 0.0) == 0) throw new ArithmeticException("Operation parameter at index " + i + " is zero. Unable to divide.");
+            result /= param;
         }
         return result;
     }
