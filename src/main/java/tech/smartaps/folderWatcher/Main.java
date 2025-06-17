62e9d70960d76d4c5cc53fc7e1a4a2bfc9a9dad2
Marvyng Ismael <assamouac20@gmail.com>
2025-06-17 09:10:21 +0000

    refactor: revert to loading configuration file directly in Main.java

--- src/main/java/tech/smartaps/folderWatcher/Main.java
+++ src/main/java/tech/smartaps/folderWatcher/Main.java
@@ -9,21 +9,15 @@ import tech.smartaps.folderWatcher.watcher.OperationFolderWatcher;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        // Retrieving configuration file
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez le chemin du fichier de configuration : ");
-        String configurationFilePath = sc.nextLine();
-
         // parsing configuration file
         Gson gson = new Gson();
-        ConfigurationModel configurationModel = WatcherHelper.parseConfigurationModel(gson, configurationFilePath);
+        ConfigurationModel configurationModel = WatcherHelper.parseConfigurationModel(gson, "configuration.json");
 
         // operation folder watchers
         List<Thread> threads = new ArrayList<>();
